c83b9c15851c10eb72988c40bc97ef3616ba5fa8
Arathy Kumar <49025521+ArathyKumar@users.noreply.github.com>
2025-02-27 00:00:26 +0530

    chore: update template (#3)

--- dev-conf-button/dev-conf-button.js
+++ dev-conf-button/dev-conf-button.js
@@ -1,41 +1,51 @@
+const template = document.createElement('template');
+template.innerHTML = `
+ <style>
+    button {
+        background-color: #8e83e4;
+        color: white;
+        border: 1px solid #7b6edf;
+        border-radius: 4px;
+        padding: 6px 12px;
+        font-size: 14px;
+        line-height: 1.42857;
+        width: 250px;
+    }
+</style>
+<button></button>
+`;
+
 class DevConfButton extends HTMLElement {
-  label = this.getAttribute('label');
-  constructor() {
-      super();
-      
-      // Attach Shadow DOM for encapsulation
-      this.attachShadow({ mode: 'open' });
+    constructor() {
+        super();
 
-      // Bind the render function to the current instance of the element
-      this.render = this.render.bind(this);
-  }
+        // Attach Shadow DOM for encapsulation
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(template.content.cloneNode(true));
+    }
 
-  connectedCallback() {
-      // Call the render function to set up the template
-      this.render();
-  }
+    // This method is called when the element is connected to the DOM
+    connectedCallback() {
+        this.updateButtonLabel();
+    }
 
-  // Render function to update the DOM
-  render() {
-      const template = `
-          <style>
-              button {
-                  background-color: #8e83e4;
-                  color: white;
-                  border: 1px solid #7b6edf;
-                  border-radius: 4px;
-                  padding: 6px 12px;
-                  font-size: 14px;
-                  line-height: 1.42857;
-                  width: 250px;
-              }
-          </style>
-          <button>${this.label}</button>
-      `;
+    // Watch for changes to the 'label' attribute
+    static get observedAttributes() {
+        return ['label'];
+    }
 
-      // Set the innerHTML of the shadow root to the template
-      this.shadowRoot.innerHTML = template;
-  }
+    // Update the button label when the 'label' attribute changes
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'label') {
+            this.updateButtonLabel();
+        }
+    }
+
+    // Function to update the button label
+    updateButtonLabel() {
+        const button = this.shadowRoot.querySelector('button');
+        button.textContent = this.getAttribute('label') || 'Default'; // Default label if no attribute
+    }
 }
 
 // Register the custom element with the browser
